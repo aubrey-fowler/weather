ac36dda04430657de2d3d0185ac2061129dc3347
Aubrey Quinn <aubreyfo@gmail.com>
2017-03-11 16:08:10 +0000

    fixed broken unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import configureStore from 'redux-mock-store';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const mockStore = configureStore([]);
+    const store = mockStore({});
+    const div = document.createElement('div');
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>, 
+        div
+    );
 });
